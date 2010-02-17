aab5302a51a85ee89d7c9c4f06693f9a2478fc74
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2010-02-17 16:59:10 +0000

    adding the Runnable class which allows you to wait for a stomp broker connection+subscription

--- lib/messenger/__init__.py
+++ lib/messenger/__init__.py
@@ -23,7 +23,7 @@ Oisin Mulvihill
 
 """
 import logging
-
+import threading
 
 def get_log():
     return logging.getLogger("messenger")
@@ -44,6 +44,54 @@ from eventutils import wait_for_event
 from eventutils import EventTimeout
 from eventutils import Catcher
 
+
+
+class Runnable(object):
+    """Provides a quick way to wait for the stomp connection
+    and broker channel subscription.
+
+    You use this class as follows:
+    
+        stomp_cfg = dict(
+            host = ..., 
+            :
+            etc
+        )
+            
+        class R(director.util.Runnable):
+            def ready(self):
+                '''Run some server / service until done'''
+
+        r = Runnabled()
+
+        #Set up the messenger stomp protocol and run with callback:
+        messenger.stompprotocol.setup(stomp_cfg, connectedOkHandler=r.connectedOk)
+        
+        # The R:ready() will be running inside its own thread, with 
+        # the mainloop reserved for twisted.
+        messenger.run(r.run)
+    
+    """
+    def __init__(self):
+        self.log = logging.getLogger('director.utils.Runnable')
+        self.isReady = threading.Event()
+        self.isExit = None
+        
+    def connectedOk(self):
+        """Called when the stop connection is ready"""
+        self.isReady.set()
+        
+    def run(self, isExit):
+        """Wait for the ready event the connectOk callback will cause.
+        """
+        self.isExit = isExit
+        get_log().info("run: Waiting for broker connection.")
+        self.isReady.wait()
+        get_log().info("run: Connection ready.")
+        self.ready()
+
+    def ready(self):
+        """Override to get called after ready event."""
 
 
 
